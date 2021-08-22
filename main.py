f39a30da0d9d24d337438b5d53dc4b9940b6bcb8
VasiliyMatlab <vasiliy0399@yandex.ru>
2021-08-22 14:09:55 +0300

    Added 'SnakeBlock' class
    - добавил класс 'SnakeBlock', содержащий в себе информацию о координатах текущего блока змейки
    - создал и отрисовал голову змейки
    - исправил ошибку в переменной 'SIZE'

--- main.py
+++ main.py
@@ -1,33 +1,51 @@
 import pygame
 
+
 SIZE_BLOCK = 20
 COUNT_BLOCKS = 20
 MARGIN = 1
 HEADER_MARGIN = 70
-SIZE = [SIZE_BLOCK * (COUNT_BLOCKS+2+MARGIN), SIZE_BLOCK * (COUNT_BLOCKS+2+MARGIN) + HEADER_MARGIN]
-print(SIZE)
+SIZE = [SIZE_BLOCK*(COUNT_BLOCKS+2) + MARGIN*COUNT_BLOCKS, SIZE_BLOCK*(COUNT_BLOCKS+2) + MARGIN*COUNT_BLOCKS + HEADER_MARGIN]
+
+SNAKE_COLOR = (0, 102, 0)
 FRAME_COLOR = (0, 255, 204)
 HEADER_COLOR = (0, 204, 153)
 WHITE = (255, 255, 255)
 BLUE = (204, 255, 255)
 
-screen = pygame.display.set_mode(SIZE)
-pygame.display.set_caption("Snake")
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
-    screen.fill(FRAME_COLOR)
-    pygame.draw.rect(screen, HEADER_COLOR, [0,0, SIZE[0],HEADER_MARGIN])
+def draw_block(color: tuple, row: int, column: int) -> None:
+    pygame.draw.rect(screen, color, [SIZE_BLOCK+column*SIZE_BLOCK+MARGIN*(column+1),HEADER_MARGIN+SIZE_BLOCK+row*SIZE_BLOCK+MARGIN*(row+1), SIZE_BLOCK,SIZE_BLOCK])
 
-    for row in range(COUNT_BLOCKS):
-        for column in range(COUNT_BLOCKS):
-            if (row + column) % 2 == 0:
-                color = BLUE
-            else:
-                color = WHITE
-            pygame.draw.rect(screen, color, [SIZE_BLOCK+column*SIZE_BLOCK+MARGIN*(column+1),HEADER_MARGIN+SIZE_BLOCK+row*SIZE_BLOCK+MARGIN*(row+1), SIZE_BLOCK,SIZE_BLOCK])
 
-    pygame.display.flip()
+class SnakeBlock:
+    def __init__(self, x, y) -> None:
+        self.x = x
+        self.y = y
+
+
+if __name__ == "__main__":
+    screen = pygame.display.set_mode(SIZE)
+    pygame.display.set_caption("Snake")
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+        screen.fill(FRAME_COLOR)
+        pygame.draw.rect(screen, HEADER_COLOR, [0,0, SIZE[0],HEADER_MARGIN])
+
+        for row in range(COUNT_BLOCKS):
+            for column in range(COUNT_BLOCKS):
+                if (row + column) % 2 == 0:
+                    color = BLUE
+                else:
+                    color = WHITE
+                draw_block(color, row, column)
+
+        snake_block = [SnakeBlock(9,9)]
+        for block in snake_block:
+            draw_block(SNAKE_COLOR, block.x, block.y)
+
+        pygame.display.flip()
